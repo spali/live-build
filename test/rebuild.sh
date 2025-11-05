806ef19a50f2e3ff73ea4add2969d5ec781e69b9
Roland Clobus <rclobus@rclobus.nl>
2025-11-05 11:53:23 +0100

    Allow offline installation with Braille support (Closes: #1116525)

--- test/rebuild.sh
+++ test/rebuild.sh
@@ -499,6 +499,8 @@ if [ ! -z "${PACKAGES}" ]; then
 	echo "${PACKAGES}" >config/package-lists/desktop.list.chroot
 	# Allow offline installation with speech enabled in the installed system #1112485
 	echo "espeakup" >config/package-lists/a11y_in_offline_installation.list.binary
+	# Allow offline installation with Braille support enabled in the installed system #1116525
+	echo "brltty" >>config/package-lists/a11y_in_offline_installation.list.binary
 fi
 
 # Set meta information about the image
